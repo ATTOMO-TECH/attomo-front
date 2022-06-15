3d1f4270e9623cd7a9d419f16e089afa27ffaaed
Daviddpc92 <david.delpozo@soamee.com>
2022-06-15 19:00:59 +0200

    fixed/Articles

--- components/slider/cases/slider.tsx
+++ components/slider/cases/slider.tsx
@@ -3,13 +3,11 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination } from 'swiper';
 import { useRef, useState, useEffect } from 'react';
 import * as qs from 'qs';
-import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { darkTheme, lightTheme, StylesArticle } from '../style';
-import { BUTTON_ACTIVE } from '../../../const/const';
 import { useUseFilterCases } from '../../../domain/useCasesDetails';
 import RenderLoading from '../../loading/loading';
 import ArticlesScrollArrow from '../arrows/arrows';
+import SlideCases from '../slide/slideCases';
 
 interface Props {
   mode: boolean;
@@ -79,34 +77,7 @@ export default function CasesScroll({ mode, filter, id, renderTouch }: Props) {
         }}>
         {data.data.map((articles: any) => (
           <SwiperSlide key={articles.attributes.company} className="swiper ">
-            <Link href={`/casos/${articles.id}`}>
-              <div className="cursor-pointer">
-                <StylesArticle.Img
-                  src={articles.attributes.mainPhoto.data[0].attributes.url}
-                  alt={
-                    articles.attributes.mainPhoto.data[0].attributes
-                      .alternativeText
-                  }
-                />
-                <StylesArticle.BlockText
-                  theme={mode === false ? lightTheme : darkTheme}>
-                  <StylesArticle.TopicText
-                    ismode={
-                      articles.attributes.mainPhoto.data[0].attributes.url ===
-                      '/'
-                        ? BUTTON_ACTIVE.OFF
-                        : BUTTON_ACTIVE.ON
-                    }>
-                    {articles.attributes.name}
-                  </StylesArticle.TopicText>
-                  <StylesArticle.TextBlog
-                    onTouchStart={() => router.push(`/casos/${articles.id}`)}
-                    ismode={mode ? BUTTON_ACTIVE.ON : BUTTON_ACTIVE.OFF}>
-                    {articles.attributes.title}
-                  </StylesArticle.TextBlog>
-                </StylesArticle.BlockText>
-              </div>
-            </Link>
+            <SlideCases articles={articles} mode={mode} />
           </SwiperSlide>
         ))}
         {data.meta.pagination.total > 2 && (
---
+++ components/slider/slide/slideCases.tsx
@@ -0,0 +1,41 @@
+import Link from 'next/link';
+import { BUTTON_ACTIVE } from '../../../const/const';
+import { StylesArticle, darkTheme, lightTheme } from '../style';
+import { handlers } from '../../../hook/longPress';
+
+interface Props {
+  articles: any;
+  mode: boolean;
+}
+
+export default function SlideCases({ articles, mode }: Props) {
+  return (
+    <>
+      <Link href={`/casos/${articles.id}`}>
+        <span {...handlers(`/casos/${articles.id}`)}>
+          <StylesArticle.Img
+            src={articles.attributes.mainPhoto.data[0].attributes.url}
+            alt={
+              articles.attributes.mainPhoto.data[0].attributes.alternativeText
+            }
+          />
+        </span>
+      </Link>
+      <StylesArticle.BlockText theme={mode === false ? lightTheme : darkTheme}>
+        <StylesArticle.TopicText
+          ismode={
+            articles.attributes.mainPhoto.data[0].attributes.url === '/'
+              ? BUTTON_ACTIVE.OFF
+              : BUTTON_ACTIVE.ON
+          }>
+          {articles.attributes.name}
+        </StylesArticle.TopicText>
+        <StylesArticle.TextBlog
+          {...handlers(`/casos/${articles.id}`)}
+          ismode={mode ? BUTTON_ACTIVE.ON : BUTTON_ACTIVE.OFF}>
+          {articles.attributes.title}
+        </StylesArticle.TextBlog>
+      </StylesArticle.BlockText>
+    </>
+  );
+}
