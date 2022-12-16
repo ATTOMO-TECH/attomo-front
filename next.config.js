7cb9e3a472944b01e75069b837e4b986bb7903e4
ivsp <ivansperez93@gmail.com>
2022-12-16 15:15:27 +0100

    Include redirect 301 in structural pages

--- next.config.js
+++ next.config.js
@@ -14,5 +14,58 @@ module.exports = {
   compiler: {
     styledComponents: true,
   },
-};
+  redirects: async () => [
+      {
+        source: '/servicios',
+        destination: '/consultora-transformacion-digital',
+        permanent: true,
+      },
+      {
+        source: '/servicios/:id',
+        destination: '/consultora-transformacion-digital/:id',
+        permanent: true,
+      },
+      {
+        source: '/casosdeexito',
+        destination: '/casos-exito-digitalizacion',
+        permanent: true,
+      },
+      {
+        source: '/casosdeexito/:id',
+        destination: '/casos-exito-digitalizacion/:id',
+        permanent: true,
+      },
+      {
+        source: '/nosotros',
+        destination: '/quienes-somos',
+        permanent: true,
+      },
+      {
+        source: '/attomo-trends',
+        destination: '/tendencias',
+        permanent: true,
+      },
+      {
+        source: '/attomo-trends/:id',
+        destination: '/tendencias/:id',
+        permanent: true,
+      },
+      {
+        source: '/careers',
+        destination: '/ofertas-trabajo',
+        permanent: true,
+      },
+      {
+        source: '/espacio-attomo',
+        destination: '/estudio-fotografia-madrid',
+        permanent: true,
+      },
+      {
+        source: '/kit-digital',
+        destination: '/agente-digitalizador-madrid',
+        permanent: true,
+      },
+    ]
+  }
+;
 
