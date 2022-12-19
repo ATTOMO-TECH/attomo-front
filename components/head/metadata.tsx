8f2bc07688535dfa538a36c062e3bccf4f3caf2b
ivsp <ivansperez93@gmail.com>
2022-12-19 14:16:24 +0100

    Fix image link in OGMT

--- components/head/metadata.tsx
+++ components/head/metadata.tsx
@@ -14,12 +14,17 @@ export function Metadata({ screen }: Props) {
         <meta name="description" content={screen.data?.attributes.metadata} />
       )}
       <meta property="og:title" content={screen.data?.attributes.screenTitle} />
-      <meta
-        property="og:description"
-        content={screen.data?.attributes.metadata}
-      />
+      {screen.data?.attributes.metadata && (
+        <meta
+          property="og:description"
+          content={screen.data?.attributes.metadata}
+        />
+      )}
       <meta property="og:url" content={document.location.origin} />
-      <meta property="og:image" content="/Favicon.svg" />
+      <meta
+        property="og:image"
+        content="https://attomo-test.s3.eu-west-3.amazonaws.com/logo_attomo_b1ea60f7cb.jpeg?updated_at=2022-12-19T13:12:57.635Z"
+      />
       <link rel="canonical" href={document.location.href} />
       <meta name="type" content="website" />
       <meta name="copyright" content={document.location.href} />
