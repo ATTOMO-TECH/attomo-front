1eb7a376827d8804665840694940bfa5e62637e0
ivsp <ivansperez93@gmail.com>
2022-12-19 15:58:08 +0100

    Include OPMT in head component

--- components/head/metadata.tsx
+++ components/head/metadata.tsx
@@ -13,6 +13,23 @@ export function Metadata({ screen }: Props) {
       {screen.data?.attributes.metadata && (
         <meta name="description" content={screen.data?.attributes.metadata} />
       )}
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={screen.data?.attributes.screenTitle} />
+      {screen.data?.attributes.metadata && (
+        <meta
+          property="og:description"
+          content={screen.data?.attributes.metadata}
+        />
+      )}
+      <meta property="og:url" content={document.location.origin} />
+      <meta
+        property="og:site_name"
+        content="ATTOMO - Consultora de Transformación Digital"
+      />
+      <meta
+        property="og:image"
+        content="https://attomo-test.s3.eu-west-3.amazonaws.com/logo_attomo_b1ea60f7cb.jpeg?updated_at=2022-12-19T13:12:57.635Z"
+      />
       <link rel="canonical" href={document.location.href} />
       <meta name="type" content="website" />
       <meta name="copyright" content={document.location.href} />
