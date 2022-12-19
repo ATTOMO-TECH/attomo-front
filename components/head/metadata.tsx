6d207b2447286f0ed33962e2d4d519ccf5eadc8e
ivsp <ivansperez93@gmail.com>
2022-12-19 17:52:25 +0100

    Include prefix in OP meta tag

--- components/head/metadata.tsx
+++ components/head/metadata.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import logoAttomo from '../../public/logo-attomo.jpg';
 
 interface Props {
   screen: any;
@@ -10,34 +11,43 @@ export function Metadata({ screen }: Props) {
       <title>{screen.data?.attributes.screenTitle}</title>
       <meta name="title" content={screen.data?.attributes.screenTitle} />
       <link rel="icon" href="/FaviconLight.svg" type="image/x-icon" />
-      {screen.data?.attributes.metadata && (
-        <meta name="description" content={screen.data?.attributes.metadata} />
-      )}
-      <meta property="og:type" content="website" />
       <meta
+        prefix="og: http://ogp.me/ns#"
+        property="og:type"
+        content="website"
+      />
+      <meta
+        prefix="og: http://ogp.me/ns#"
         property="og:title"
         content="ATTOMO - Consultora de Transformación Digital"
       />
-      {screen.data?.attributes.metadata && (
-        <meta
-          property="og:description"
-          content="* ATTOMO - Consultora de Transformación Digital *
-          Ayudamos a las empresas privadas y administraciones públicas en todo su proceso de digitalización."
-        />
-      )}
-      <meta property="og:url" content="https://www.attomo.digital" />
       <meta
+        prefix="og: http://ogp.me/ns#"
+        property="og:description"
+        content=" ATTOMO - Consultora de Transformación Digital - Ayudamos a las empresas privadas y administraciones públicas en todo su proceso de digitalización."
+      />
+      <meta
+        prefix="og: http://ogp.me/ns#"
+        property="og:url"
+        content="https://www.attomo.digital"
+      />
+      <meta
+        prefix="og: http://ogp.me/ns#"
         property="og:site_name"
         content="ATTOMO - Consultora de Transformación Digital"
       />
       <meta
+        prefix="og: http://ogp.me/ns#"
         property="og:image"
-        content="https://attomo-test.s3.eu-west-3.amazonaws.com/logo_attomo_b1ea60f7cb.jpeg?updated_at=2022-12-19T13:12:57.635Z"
+        content={`${logoAttomo}`}
       />
       <link rel="canonical" href={document.location.href} />
       <meta name="type" content="website" />
       <meta name="copyright" content={document.location.href} />
       <meta name="robots" content="index" />
+      {screen.data?.attributes.metadata && (
+        <meta name="description" content={screen.data?.attributes.metadata} />
+      )}
     </Head>
   );
 }
