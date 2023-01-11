82230e167c84ce725507aba0ef616c9a8abeaced
ivsp <ivansperez93@gmail.com>
2023-01-11 12:47:04 +0100

    Include og:image tag in metadata component

--- components/head/metadata.tsx
+++ components/head/metadata.tsx
@@ -21,9 +21,11 @@ export function Metadata({ screen }: Props) {
       )}
       <meta property="og:url" content={document.location.origin} />
       <meta
-        property="og:image"
-        content="https://attomo-test.s3.eu-west-3.amazonaws.com/logo_attomo_b1ea60f7cb.jpeg?updated_at=2022-12-19T13:12:57.635Z"
+        property="og:image:secure_url"
+        content="https://attomo-test.s3.eu-west-3.amazonaws.com/logo_attomo_og_image_e0ce1b754e.jpeg?updated_at=2023-01-11T11:41:23.681Z"
       />
+      <meta property="og:image:width" content="500" />
+      <meta property="og:image:height" content="500" />
       <link rel="canonical" href={document.location.href} />
       <meta name="type" content="website" />
       <meta name="copyright" content={document.location.href} />
