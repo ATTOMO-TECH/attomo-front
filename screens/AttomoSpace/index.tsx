9238198f923023f3c42a0f50ae2bce3a9f040b7b
MiguelSiestoGarabana <miguel@attomo.digital>
2026-03-27 17:11:43 +0100

    Rama de despliegue: limpieza de páginas sin contenido en Strapi

--- screens/AttomoSpace/index.tsx
+++ screens/AttomoSpace/index.tsx
@@ -1,4 +1,3 @@
-import dynamic from 'next/dynamic';
 import { useState, useEffect } from 'react';
 import Footer from '../../components/footer/footer';
 import Menu from '../../components/nav/menu';
@@ -8,26 +7,14 @@ import Title from '../../components/Text/title';
 import ParagraphText from '../../components/Text/paragraphText';
 import { BUTTON_ACTIVE } from '../../const/const';
 import { Styles } from '../../styles/styles';
-import BlockSection from '../../components/block/block';
 import SelectedClients from '../../components/section/selectedclientes';
 import FormReserver from '../../components/form/formReserver';
 import ButtonShare from '../../components/button/BtnShare';
 import MapsBlock from '../../components/maps/maps';
 import { getLocale } from '../../public/locales/getLocale';
-import { BGSPACE, VALUESNAV_ENG, VALUESNAV_ESP } from '../../const/constGlobal';
 import { Props } from '../types';
-import useDeviceSize from '../../hook/size';
 
 function Space({ locale }: Props) {
-  const [width] = useDeviceSize();
-  const SliderSSR = dynamic(
-    () =>
-      import('../../components/slider/espacio/slider').then(
-        (module: any) => module.default,
-      ),
-    { ssr: false },
-  );
-
   const [translate, setTranslate] = useState(getLocale(locale));
 
   useEffect(() => {
@@ -60,7 +47,7 @@ function Space({ locale }: Props) {
 
         <Styles.Center>
           <Styles.ScreenWS>
-            {translate.spaceAttomo.map((services) => (
+            {translate.spaceAttomo?.map((services) => (
               <Styles.BlockDiv key={services.Text}>
                 <MainTitle size="lg:pr-10 lg:text-4xl md:text-3xl pb-2 text-2xl">
                   {services.Text}
@@ -77,7 +64,7 @@ function Space({ locale }: Props) {
         <Styles.HeroEspace />
         <Styles.Center>
           <Styles.BlockSelected>
-            {translate.bookingAttomo.map((values) => (
+            {translate.bookingAttomo?.map((values) => (
               <SelectedClients
                 key={values.Text}
                 text={values.Subtext}
@@ -86,18 +73,6 @@ function Space({ locale }: Props) {
               />
             ))}
           </Styles.BlockSelected>
-          {width > 768 ? (
-            <Styles.SectionImg>
-              {BGSPACE.map((imageSpace: any) => (
-                <Styles.BlockImg
-                  className={imageSpace.img}
-                  key={imageSpace.id}
-                />
-              ))}
-            </Styles.SectionImg>
-          ) : (
-            <SliderSSR />
-          )}
         </Styles.Center>
         <Styles.Center id="reserva">
           <FormReserver locale={locale} />
@@ -114,21 +89,6 @@ function Space({ locale }: Props) {
         <Styles.BlockAddresMapImg>
           {typeof window !== 'undefined' && <MapsBlock />}
         </Styles.BlockAddresMapImg>
-        <Styles.Center>
-          {translate.contact.map((values) => (
-            <BlockSection
-              key={values.Link}
-              text={values.Text}
-              button={values.Link}
-              text2=""
-              button2=""
-              mode
-              link={
-                locale === 'en' ? VALUESNAV_ENG[5].Url : VALUESNAV_ESP[5].Url
-              }
-            />
-          ))}
-        </Styles.Center>
 
         <Footer subFooter={false} />
       </Styles.Body>
